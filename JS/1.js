932e24e19078b219a9c330baba9f474d70703768
Rishi Rana <rishirana012354@gmail.com>
2026-02-27 11:29:36 +0530

    feat: Add function to check for palindromes and count vowels; update main component to include Des

--- JS/1.js
+++ JS/1.js
@@ -29,18 +29,64 @@
 
 
 
-let score = 65;
+// let score = 65;
 
-switch (score) {
-    case (score >= 90):
-        console.log("Grade A");
-        break;
-    case (score >= 80):
-        console.log("Grade : B");
-        break;
-    case (score >= 70):
-        console.log("Grade : C");
-        break;
-    default:
-        console.log("Firr se Pdoooo Ja Krrr........ Nikloooooooooo///");
+// switch (score) {
+//     case (score >= 90):
+//         console.log("Grade A");
+//         break;
+//     case (score >= 80):
+//         console.log("Grade : B");
+//         break;
+//     case (score >= 70):
+//         console.log("Grade : C");
+//         break;
+//     default:
+//         console.log("Firr se Pdoooo Ja Krrr........ Nikloooooooooo///");
+// }
+
+
+// function processNumbers(nums) {
+//     // 1. Remove duplicates 
+//     let uniqueNums = [...new Set(nums)];
+
+//     // 2. Sort 
+//     uniqueNums.sort((a, b) => a - b);
+
+//     console.log("Unique:", uniqueNums);
+
+//     //  second largest number
+//     if (uniqueNums.length >= 2) {
+//         let secondLargest = uniqueNums[uniqueNums.length - 2];
+//         console.log("Second Largest Number:", secondLargest);
+//     } else {
+//         console.log("Not enough unique numbers to find the second largest.");
+//     }
+// }
+
+// const myNumbers = [10, 20, 4, 45, 99, 20, 99, 4];
+// processNumbers(myNumbers);
+
+function program(str) {
+    // 1.  Palindrome
+    let cleanStr = str.toLowerCase().replace(/[^a-z0-9]/g, ""); 
+    let reversedStr = cleanStr.split("").reverse().join("");
+    let isPalindrome = (cleanStr === reversedStr);
+
+    // 2. Count Vowels
+    let vowelCount = 0;
+    const vowels = "aeiou";
+
+    for (let char of str.toLowerCase()) {
+        if (vowels.includes(char)) {
+            vowelCount++;
+        }
+    }
+
+    console.log(`Original String: ${str}`);
+    console.log(`Is it a Palindrome?: ${isPalindrome}`);
+    console.log(`Number of Vowels: ${vowelCount}`);
 }
+
+program("Rishi Rana");
+program("MAM");
--- React/Hooks-Practice/use-state/src/main.jsx
+++ React/Hooks-Practice/use-state/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import Api from './practice/Api.jsx'
+import Des from './des.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
